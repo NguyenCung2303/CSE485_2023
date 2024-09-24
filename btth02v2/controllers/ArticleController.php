57e677109323f1b0feaa4bbee2343a86b74852d5
quocanh4456 <quocanh4456@gmail.com>
2024-09-25 00:18:25 +0700

    qa upload

--- btth02v2/controllers/ArticleController.php
+++ btth02v2/controllers/ArticleController.php
@@ -1,17 +1,16 @@
 <?php
-
-class CategoryController{
+class ArticleController{
     // Hàm xử lý hành động index
     public function index(){
         // Nhiệm vụ 1: Tương tác với Services/Models
-        echo "Tương tác với Services/Models from Category";
+        echo "Tương tác với Services/Models from Article";
         // Nhiệm vụ 2: Tương tác với View
-        echo "Tương tác với View from Category";
+        echo "Tương tác với View from Article";
     }
 
     public function add(){
         // Nhiệm vụ 1: Tương tác với Services/Models
-        // echo "Tương tác với Services/Models from Category";
+        // echo "Tương tác với Services/Models from Article";
         // Nhiệm vụ 2: Tương tác với View
         include("views/article/add_article.php");
     }
